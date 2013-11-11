490373bd1bee3111809abfa0aa323b0edb62378f
Mostafa Eweda <mostafa@c9.io>
2013-11-11 18:58:22 +0200

    Previous: * [Untested] Collab members & access request API * [Untested] Client API plugin for any usage of API authenticated http requests * [Untested] WorkspaceMember model feature functions (mapping to redis) * Members panel above chat using ace_tree (not complete yet) * Share dialog * Merge master & fix conflicts This commit: * nodeBin & nodePath client configs (0.10) * Hosted version now initially has collab * Apply optimize PRs c9/c9+506 & c9/c9+511 * Abstract more OT document logic

--- watches.js
+++ watches.js
@@ -134,7 +134,7 @@ define(function(require, exports, module) {
                 menus.addItemToMenu(menu, new ui.item({
                     caption : "Add As Watch Expression",
                     command : "addwatchfromselection"
-                }), 50, plugin);
+                }), 600, plugin);
             });
             
             // restore the variables from the IDE settings
