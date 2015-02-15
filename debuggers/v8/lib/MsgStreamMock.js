8421e5e3a5f73f705d81790bacda822091dc510e
nightwing <amirjanyan@gmail.com>
2015-02-15 22:29:07 +0400

    fix remaining license comments

--- debuggers/v8/lib/MsgStreamMock.js
+++ debuggers/v8/lib/MsgStreamMock.js
@@ -2,7 +2,6 @@
  * Ajax.org Code Editor (ACE)
  *
  * @copyright 2010, Ajax.org Services B.V.
- * @license LGPLv3 <http://www.gnu.org/licenses/lgpl-3.0.txt>
  * @author Fabian Jakobs <fabian AT ajax DOT org>
  */
  
